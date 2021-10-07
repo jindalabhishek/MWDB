2b6e294a644e73bc09151fafce981a257d88b003
dhruv <dhruvagja8808@gmail.com>
2021-10-07 00:26:42 -0700

    Added PCA and truncated_SVD with menu

--- Phase_2/main_task_2.py
+++ Phase_2/main_task_2.py
@@ -1,7 +1,7 @@
 from Util.dao_util import DAOUtil
 from numpy.linalg import svd
 from sklearn.decomposition import TruncatedSVD
-
+from dimention_reduction_util import *
 
 def get_image_vector_matrix(feature_descriptors, feature_model):
     image_vector_matrix = []
@@ -13,6 +13,7 @@ def get_image_vector_matrix(feature_descriptors, feature_model):
 def main():
     """
         Executes Task 2
+        Output Subject - latent semantics matrix, (subject-list of weight matrix)
     """
     """
         Connection to MongoDB using PyMongo
@@ -23,14 +24,15 @@ def main():
     subject_id = input('Enter Subject Id:')
     feature_descriptors = dao_util.get_feature_descriptors_by_subject_id(subject_id)
     image_vector_matrix = get_image_vector_matrix(feature_descriptors, feature_model)
-    print(image_vector_matrix)
-    # for row in image_vector_matrix:
-    #     print(len(row))
-    svd = TruncatedSVD(n_components=2)
-    svd.fit_transform(image_vector_matrix)
-    print(svd.singular_values_)
-    # print(VT.shape)
-    # print(VT)
+    dimension_reduction_technique = input('Select Dimension reduction technique: (1. PCA 2.SVD 3.LDA 4.k-means): ')
+    print('Image_vector_matrix dimension: ', len(image_vector_matrix),len(image_vector_matrix[0]))
+    if dimension_reduction_technique == '1':
+        subject_weight_matrix = get_reduced_matrix_using_pca(image_vector_matrix, 13)
+    elif dimension_reduction_technique == '2':
+        subject_weight_matrix = get_reduced_matrix_using_svd(image_vector_matrix, 13)
+
+    print('Subject_weight_matrix dimension', len(subject_weight_matrix), len(subject_weight_matrix[0]))
+    print('Entrire Subject weight matrix: \n', subject_weight_matrix)
 
 
 main()
