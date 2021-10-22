2c7e2aa411c1e590dee46505a761cc825c8a830e
VinhTA-Computer-Master <75757727+VinhTA-Computer-Master@users.noreply.github.com>
2021-10-22 16:03:42 -0700

    Update main_task_1.py

--- Phase_2/main_task_1.py
+++ Phase_2/main_task_1.py
@@ -14,34 +14,72 @@ def get_image_vector_matrix(feature_descriptors, feature_model):
     return image_vector_matrix, image_types
 
 
-def main():
+def task1():
     """
-        Executes Task 2
+        Executes Task 1
         Output Subject - latent semantics matrix, (subject-list of weight matrix)
     """
     """
         Connection to MongoDB using PyMongo
     """
-    dao_util = DAOUtil()
-    # feature_model = input('Welcome to Task 1 Demo. Enter the feature model:')
-    feature_model = 'hog'
-    feature_model += '_feature_descriptor'
-    # type_id = input('Enter Type Id:')
-    type_id = 'cc'
-    feature_descriptors = dao_util.get_feature_descriptors_by_type_id(type_id)
-    image_vector_matrix, image_types = get_image_vector_matrix(feature_descriptors, feature_model)
-    # dimension_reduction_technique = input('Select Dimension reduction technique: (1. PCA 2.SVD 3.LDA 4.k-means): ')
-    dimension_reduction_technique = '1'
-    print('Image_vector_matrix dimension: ', len(image_vector_matrix), len(image_vector_matrix[0]))
-    image_vector_matrix = numpy.array(image_vector_matrix)
-    subject_weight_matrix = None
-    if dimension_reduction_technique == '1':
-        subject_weight_matrix = get_reduced_matrix_using_pca(image_vector_matrix, 13)
-    elif dimension_reduction_technique == '2':
-        subject_weight_matrix = get_reduced_matrix_using_svd(image_vector_matrix, image_types, 13)
+    
+    ##### Acquire parameters from user #####
+    feature = input("Enter feature model: ")
+    while not (feature in ["color", "lbp", "hog"]):
+      print("Invalid input - choose from \"color\", \"lbp\", \"hog\"")
+      feature = input("Enter feature model: ")
 
-    # print('Subject_weight_matrix dimension', len(subject_weight_matrix), len(subject_weight_matrix[0]))
-    print('Entire Subject weight matrix: \n', subject_weight_matrix)
+    type_list = ['cc', 'con', 'emboss', 'jitter', 'neg',\
+	  'noise01', 'noise02', 'original', 'poster', 'rot',\
+	  'smooth', 'stipple']
+    img_type = input("Enter image type: ")
+    while not (img_type in type_list):
+      print("Invalid input - choose from \"cc\", \"con\", \"detail\", \"emboss\", \"jitter\", \"neg\", \"noise1\", \"noise2\", \"original\", \"poster\", \"rot\", \"smooth\", \"stipple\"")
+      img_type = input("Enter image type: ")
 
+    k = int(input("Enter k value: "))
+    
+    reduction_technique = input("Enter dimensionality reduction technique: ")
+    while not (reduction_technique in ["pca", "svd", "lda", "kmeans"]):
+      print("Invalid input - choose from \"pca\", \"svd\", \"lda\", \"kmeans\"")
+      reduction_technique = input("Enter dimensionality reduction technique: ")
 
-main()
+    ##### Extract and preprocess data matrix #####
+    db = DAOUtil()
+    images = db.get_feature_descriptors_by_type_id(img_type)
+
+    if(feature == 'color'):
+      feature_extracted_dataset = np.zeros((len(images),192))
+      for i in range(len(images)):
+        feature_extracted_dataset[i] = np.array(images[i]['color_moment_feature_descriptor'])
+    elif(feature == 'lbp'):
+      feature_extracted_dataset = np.zeros((len(images),26))
+      for i in range(len(images)):
+        feature_extracted_dataset[i] = np.array(images[i]['elbp_feature_descriptor'])
+    else:
+      feature_extracted_dataset = np.zeros((len(images),1764))
+      for i in range(len(images)):
+        feature_extracted_dataset[i] = np.array(images[i]['hog_feature_descriptor'])
+
+    if reduction_technique == 'pca':
+        subject_weight_matrix = get_reduced_matrix_using_pca(feature_extracted_dataset, k)
+    elif reduction_technique == 'svd':
+        subject_weight_matrix = get_reduced_matrix_using_svd(feature_extracted_dataset, k)
+    elif reduction_technique == 'lda':
+        subject_weight_matrix = get_reduced_matrix_using_lda(feature_extracted_dataset, k)
+    elif reduction_technique == 'kmeans':
+        subject_weight_matrix = get_reduced_matrix_using_kmeans(feature_extracted_dataset, k)
+        
+    subject_weight_pairs = np.zeros((40,k))
+
+    for latent_feature in range(k):
+      sum_subject = np.zeros((40,2))
+      for i in range(len(images)):
+        subject_id = int(images[i]['label'].split('-',3)[2])-1
+        sum_subject[subject_id][0] += latent_features_dataset[i][latent_feature]
+        sum_subject[subject_id][1] += 1
+      for i in range(40):
+        subject_weight_pairs[i][latent_feature] = sum_subject[i][0]/sum_subject[i][1]
+    return subject_weight_pairs
+
+subject_weight_pairs = task1()
