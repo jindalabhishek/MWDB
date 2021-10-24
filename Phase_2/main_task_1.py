3cd9e7797dfc263100bd9e56d1940ff15df41110
VinhTA-Computer-Master <75757727+VinhTA-Computer-Master@users.noreply.github.com>
2021-10-23 23:58:55 -0700

    Update main_task_1.py

--- Phase_2/main_task_1.py
+++ Phase_2/main_task_1.py
@@ -29,7 +29,7 @@ def main():
         Connection to MongoDB using PyMongo
     """
     dao_util = DAOUtil()
-    # feature_model = input('Welcome to Task 1 Demo. Enter the feature model:')
+    # feature_model = input('Welcome to Task 1 Demo. Enter the feature model (color_moment, elbp, hog):')
 
     feature_model = 'hog'
     feature_model_name = feature_model
