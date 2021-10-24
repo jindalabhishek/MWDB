bf4f41c7ecf4893eba34e30a3f9b339904445739
swamirishi <47532440+swamirishi@users.noreply.github.com>
2021-10-23 19:43:56 -0700

    PCA Fix (#10)
    Co-authored-by: Swaminathan Balachandran <swamirishi@Swaminathans-MacBook-Pro.local>

--- Phase_2/main_task_2.py
+++ Phase_2/main_task_2.py
@@ -1,78 +1,60 @@
+import numpy
+
+from Phase_2.dimensionality_reduction.KMeans import KMeans
+from Phase_2.dimensionality_reduction.LDA import LDA
+from Phase_2.dimensionality_reduction.PCA import PCA
+from Phase_2.dimensionality_reduction.SVD import SVD
+from Util.Utils import get_output_file_path
 from Util.dao_util import DAOUtil
 from numpy.linalg import svd
-from sklearn.decomposition import TruncatedSVD
-from sklearn.decomposition import LatentDirichletAllocation
-from Util.k_means_util import reduce_dimensions_k_means
 import numpy as np
-
-from sklearn.decomposition import LatentDirichletAllocation
-from Util.k_means_util import reduce_dimensions_k_means
-import numpy as np
+from Util.json_util import LatentSemanticFile
 
 
 def get_image_vector_matrix(feature_descriptors, feature_model):
     image_vector_matrix = []
+    image_subjects = []
     for feature_descriptor in feature_descriptors:
         image_vector_matrix.append(feature_descriptor[feature_model])
-    return image_vector_matrix
-
-
-def get_image_label_array(feature_descriptors):
-    image_label_array = []
-    for feature_descriptor in feature_descriptors:
-        label = feature_descriptor['label']
-        image_type = label.split('-')[1]
-        image_label_array.append(image_type)
-        # image_label_array.append(i)
-    return image_label_array
-
-
-def calculate_lda(image_vector_matrix, image_types, components):
-    lda = LatentDirichletAllocation(n_components=components)
-    image_vector_matrix_lda = lda.fit_transform(image_vector_matrix, image_types)
-    return image_vector_matrix_lda
-
-
-def normalize_data_for_lda(image_vector_matrix):
-    normalized_data = (image_vector_matrix - np.min(image_vector_matrix)) \
-                      / (np.max(image_vector_matrix) - np.min(image_vector_matrix))
-    return normalized_data
+        image_subjects.append(feature_descriptor['label'].split("-")[1])
+    return image_vector_matrix, image_subjects
 
 
 def main():
     """
-        Executes Task 2
-        Output Subject - latent semantics matrix, (subject-list of weight matrix)
+        Executes Task 1
+        Output type - latent semantics matrix, (type-list of weight matrix)
     """
     """
         Connection to MongoDB using PyMongo
     """
     dao_util = DAOUtil()
-    feature_model = input('Welcome to Task 2 Demo. Enter the feature model (color_moment, elbp, hog):')
+    # feature_model = input('Welcome to Task 1 Demo. Enter the feature model:')
+    feature_model = 'color_moment'
     feature_model += '_feature_descriptor'
-    subject_id = input('Enter Subject Id (Y):')
-    dimension_reduction_technique = input('Enter dimension reduction technique (pca, svd, lda, kmeans):')
-    k = int(input('Enter value of k:'))
+    # subject_id = input('Enter Subject Id:')
+    subject_id = '1'
     feature_descriptors = dao_util.get_feature_descriptors_by_subject_id(subject_id)
-    image_vector_matrix = get_image_vector_matrix(feature_descriptors, feature_model)
-    image_types = get_image_label_array(feature_descriptors)
-    max_n_components = len(set(image_types))
-    print("Max N Components:", max_n_components)
-    k = min(k, max_n_components)
-    image_vector_matrix_k_dimensions = None
-    if dimension_reduction_technique == 'pca':
+    image_vector_matrix, image_subjects = get_image_vector_matrix(feature_descriptors, feature_model)
+    # dimension_reduction_technique = input('Select Dimension reduction technique: (1. PCA 2.SVD 3.LDA 4.k-means): ')
+    dimension_reduction_technique = '4'
+    print('Image_vector_matrix dimension: ', len(image_vector_matrix), len(image_vector_matrix[0]))
+    image_vector_matrix = numpy.array(image_vector_matrix)
+    reductionTechniqueDict = {'1':PCA(),'2':SVD(),'3':LDA(),'4':KMeans(1000)}
+    type_weight_matrix = reductionTechniqueDict[dimension_reduction_technique].compute(image_vector_matrix,13,image_subjects)
+    type_weight_pairs = {}
+    for i in range(len(image_subjects)):
+        image_label = image_subjects[i]
+        if image_label not in type_weight_pairs:
+            type_weight_pairs[image_label] = []
+        type_weight_pairs[image_label].append(type_weight_matrix[i])
 
-    elif dimension_reduction_technique == 'svd':
+    for i in type_weight_pairs:
+        type_weight_pairs[i] = np.average(np.array(type_weight_pairs[i]),axis=0).tolist()
 
-    elif dimension_reduction_technique == 'lda':
-        normalized_data = normalize_data_for_lda(np.array(image_vector_matrix))
-        image_vector_matrix_k_dimensions = calculate_lda(normalized_data, image_types, k)
-    elif dimension_reduction_technique == 'kmeans':
-        image_vector_matrix_k_dimensions = reduce_dimensions_k_means(image_vector_matrix,
-                                                                     n_components=k, n_iterations=1000)
-    print(image_vector_matrix_k_dimensions.shape)
-    # print(len(image_vector_matrix_k_dimensions))
-    # print(len(image_vector_matrix_k_dimensions[0]))
-
+    LatentSemanticFile(feature_model,reductionTechniqueDict[dimension_reduction_technique],type_weight_pairs)\
+        .serialize(get_output_file_path(2,feature_model,subject_id,type(reductionTechniqueDict[dimension_reduction_technique]).__name__))
+    # print('type_weight_matrix dimension', len(type_weight_matrix), len(type_weight_matrix[0]))
+    print('Entire type weight matrix: \n', type_weight_matrix)
 
 main()
